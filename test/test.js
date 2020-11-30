d24bf02869be9b7790b4a1de89e857b641303b6b
paulmboyce <paul.boyce@goobito.com>
2020-11-30 13:04:07 +0000

    refactor(app): add mocha and chai for unit testing. - make test run mocha with --watch flag for realtime test status - move code to /src - make runAnalysis both load ad a module and run in browser, both with test data and real data

--- test/test.js
+++ test/test.js
@@ -1,8 +1,27 @@
-var assert = require("assert");
+var score = require("../src/score");
+var expect = require("chai").expect;
+
 describe("Array", function () {
-  describe("#indexOf()", function () {
-    it("should return -1 when the value is not present", function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
+	describe("#indexOf()", function () {
+		it("should return -1 when the value is not present", function () {
+			expect([1, 2, 3].indexOf(4)).to.equal(-1);
+		});
+
+		it("should run analysis", function () {
+			expect(true).to.equal(true);
+		});
+
+		it("should load the analysis", function () {
+			const DATA = [
+				[10, 0.5, 16, 1],
+				[200, 0.5, 16, 4],
+				[650, 0.5, 16, 4],
+				[300, 0.5, 16, 5],
+			];
+			console.log("AN", score.runAnalysis);
+			const result = score.runAnalysis(DATA);
+			console.log(result);
+			expect(result).to.equal(5);
+		});
+	});
 });
